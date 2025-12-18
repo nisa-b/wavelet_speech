a4ca1da5f1368e57eb32d8db8f10e9c4f38c89e6
nisa-b <nisa.input@gmail.com>
2025-12-18 09:48:16 +0300

    Update features.py

--- features.py
+++ features.py
@@ -32,7 +32,8 @@ def load_class(folder, label):
             energy_CD = np.sum(cD**2)
             ratio = energy_CD / (energy_CA + 1e-12)
 """
-Eğer bu oran (ratio) yüksekse, enerji "Detay (yüksek frekans)" katsayılarına daha çok dağılmış demektir. Bu da o sesin daha sürtünmeli veya gürültülü (örneğin "f", "s", "h" gibi harfler içeren) bir ses olduğunu makineye anlatır.
+Eğer bu oran (ratio) yüksekse, enerji "Detay (yüksek frekans)" katsayılarına daha çok dağılmış demektir. 
+Bu da o sesin daha sürtünmeli veya gürültülü (örneğin "f", "s", "h" gibi harfler içeren) bir ses olduğunu makineye anlatır.
 """
             entropy_CA = shannon_entropy(cA)
             entropy_CD = shannon_entropy(cD)
@@ -60,4 +61,5 @@ def load_dataset():
 
     print("Feature shape:", X.shape)
     return X, y
+
 
